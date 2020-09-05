bddfa51e6f09703f88cd4f58c711d4172f58a40f
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-05 14:09:53 -0300

    Aumentando a quantidade

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -8,15 +8,22 @@ const INITIAL_STATE: ICartState = {
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
    return produce(state, draft => { 
-    switch(action.type) {
-        case 'ADD_PRODUCT_TO_CART': {
-            const {product} = action.payload;
+        switch(action.type) {
+            case 'ADD_PRODUCT_TO_CART': {
+                const {product} = action.payload;
 
-            
+                const productInCartIndex = draft.items.findIndex(item => 
+                        item.product.id === product.id,
+                );
+
+                if(productInCartIndex >= 0) {
+                    draft.items[productInCartIndex].quantity++;
+                } else {
                 draft.items.push({
                     product,
                     quantity: 1,
                 });
+                }
             
                 break;
             // return {
