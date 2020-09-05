d1240ac074e14d88bc5eec0a9671f6b7d8e2d2fb
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-05 14:06:03 -0300

    Utilizando a lib immer para trabalhar com imutabilidade

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
@@ -6,29 +7,35 @@ const INITIAL_STATE: ICartState = {
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-
+   return produce(state, draft => { 
     switch(action.type) {
         case 'ADD_PRODUCT_TO_CART': {
             const {product} = action.payload;
 
             
-
-            return {
-                ...state,
-                items: [
-                    ...state.items,
-                    {
-                        product,
-                        quantity: 1,
-                    }
-                ]
-            };
+                draft.items.push({
+                    product,
+                    quantity: 1,
+                });
+            
+                break;
+            // return {
+            //     ...state,
+            //     items: [
+            //         ...state.items,
+            //         {
+            //             product,
+            //             quantity: 1,
+            //         }
+            //     ]
+            // };
             
         }
         default: {
-            return state;
+            return draft;
         }
     }
+    });
 
 }
 export default cart;
